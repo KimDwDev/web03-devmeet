62a4176bb1182c34b130f8e5d6c0ec1ef45324f2
tjsdn052 <tjsdn052@naver.com>
2025-12-18 19:03:22 +0900

    📍 Feat: sidebar 텍스트 버튼 클릭시 나타날 textpanel 작성
    - 동일 버튼 클릭시 탭 닫히도록 TabType null 추가
    
    - toggleTab 추가
    
    - 텍스트 패널 추가

--- frontend/src/components/workspace/Sidebar.tsx
+++ frontend/src/components/workspace/Sidebar.tsx
@@ -8,8 +8,9 @@ import { TextItem, ImageItem, VideoItem } from '@/types/workspace';
 
 import NavButton from './sidebar/NavButton';
 import CardPanel from './sidebar/card/CardPanel';
-import ImagePanel from './sidebar/image/ImagePanel';
+import TextPanel from './sidebar/TextPanel';
 
+type TabType = 'card' | 'text' | 'image' | 'video' | null;
 
 export default function Sidebar() {
   const [activeTab, setActiveTab] = useState<TabType>('card');
@@ -40,6 +41,12 @@ export default function Sidebar() {
     window.addEventListener('keydown', handleKeyDown);
     return () => window.removeEventListener('keydown', handleKeyDown);
   }, [cardData]);
+
+  const toggleTab = (tab: TabType) => {
+    setActiveTab((prev) => (prev === tab ? null : tab));
+  };
+
+  // TODO : 각 아이템 추가 핸들러 구현
 
   return (
     <aside className="z-1 flex h-full border-r border-neutral-200 bg-white shadow-sm">
