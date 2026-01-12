fb07c8111d16cf4f7ee7ec4ef5b866064bfcb07c
seorang42 <seorang42@naver.com>
2026-01-13 01:24:19 +0900

    🔨 Fix: 빌드 오류 해결
    useSearchParams를 Suspense로 감싸지 않고 사용하여 발생하는 빌드 오류를 수정하였습니다

--- frontend/src/app/(app)/auth/error/page.tsx
+++ frontend/src/app/(app)/auth/error/page.tsx
@@ -1,29 +1,36 @@
 'use client';
 
+import { Suspense } from 'react';
+import { useSearchParams, useRouter } from 'next/navigation';
 import Button from '@/components/common/button';
-import { useRouter, useSearchParams } from 'next/navigation';
 
-export default function AuthErrorPage() {
+function ErrorContent() {
   const searchParams = useSearchParams();
+  const router = useRouter();
 
   const status = searchParams.get('status');
   const message = searchParams.get('message');
-
-  const router = useRouter();
   const onClick = () => router.replace('/landing');
 
   return (
-    <main className="flex-center min-h-screen w-screen">
-      <div className="flex w-full max-w-90 flex-col gap-8 px-6 py-4">
-        <section className="flex flex-col items-center gap-4">
-          <h1 className="section-title">로그인 실패</h1>
-          <span className="text-center whitespace-pre-wrap text-neutral-600">
-            {`상태 코드: ${status}\n${message}`}
-          </span>
-        </section>
+    <div className="flex w-full max-w-90 flex-col gap-8 px-6 py-4">
+      <section className="flex flex-col items-center gap-4">
+        <h1 className="section-title">로그인 실패</h1>
+        <span className="text-center whitespace-pre-wrap text-neutral-600">
+          {`상태 코드: ${status}\n${message}`}
+        </span>
+      </section>
+      <Button onClick={onClick}>홈으로</Button>
+    </div>
+  );
+}
 
-        <Button onClick={onClick}>홈으로</Button>
-      </div>
+export default function AuthErrorPage() {
+  return (
+    <main className="flex-center min-h-screen w-screen">
+      <Suspense fallback={<></>}>
+        <ErrorContent />
+      </Suspense>
     </main>
   );
 }
