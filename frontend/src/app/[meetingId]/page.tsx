2dab973727a883fac9bc0aacefd333287af472be
seorang42 <seorang42@naver.com>
2026-01-15 15:38:53 +0900

    📍 Feat: 회의 참가 시 비밀번호 로직 구현
    흐름만 구현되었고, 실제 검증 로직은 API 구현 후 연결될 예정입니다.

--- frontend/src/app/[meetingId]/page.tsx
+++ frontend/src/app/[meetingId]/page.tsx
@@ -1,20 +1,51 @@
 'use client';
 
+import { DUMMY_MEETING_INFO } from '@/app/[meetingId]/dummy';
+import Modal from '@/components/common/Modal';
 import MeetingLobby from '@/components/meeting/MeetingLobby';
 import MeetingRoom from '@/components/meeting/MeetingRoom';
 import { useMeetingSocket } from '@/hooks/useMeetingSocket';
 import { useParams } from 'next/navigation';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
 export default function MeetingPage() {
   const { socket } = useMeetingSocket();
 
+  // 이후 실제 회의 정보 API 호출로 수정 필요
+  const { password } = DUMMY_MEETING_INFO;
+
   const params = useParams<{ meetingId: string }>();
   const meetingId = params.meetingId;
 
+  const passwordRef = useRef<HTMLInputElement>(null);
+  const [isPasswordModalOpen, setIsPasswordModalOpen] = useState(false);
+  const [isPasswordError, setIsPasswordError] = useState(false);
   const [isJoined, setIsJoined] = useState<boolean>(false);
 
-  // 참여 버튼을 눌렀을 때의 핸들러
+  // 회의실 비밀번호 유무로 분기 처리
+  const validateJoin = () => {
+    if (password) {
+      setIsPasswordModalOpen(true);
+    } else {
+      handleJoin();
+    }
+  };
+
+  // 비밀번호 검증
+  const onPasswordConfirm = () => {
+    const value = passwordRef.current?.value;
+
+    // 비밀번호 검증 API 호출 필요
+    if (value === password) {
+      setIsPasswordModalOpen(true);
+      handleJoin();
+    } else {
+      setIsPasswordModalOpen(false);
+      setIsPasswordError(true);
+    }
+  };
+
+  // 회의실 참여 로직
   const handleJoin = () => {
     setIsJoined(true);
   };
@@ -26,7 +57,34 @@ export default function MeetingPage() {
   return (
     <main className="min-h-screen">
       {!isJoined ? (
-        <MeetingLobby meetingId={meetingId} onJoin={handleJoin} />
+        <>
+          <MeetingLobby meetingId={meetingId} onJoin={validateJoin} />
+          {isPasswordModalOpen && (
+            <Modal
+              title="비밀번호 입력"
+              cancelText="취소"
+              onCancel={() => setIsPasswordModalOpen(false)}
+              confirmText="확인"
+              onConfirm={onPasswordConfirm}
+              isLightMode
+            >
+              <input ref={passwordRef} className="input-sm input-light" />
+            </Modal>
+          )}
+          {isPasswordError && (
+            <Modal
+              title="입장 실패"
+              cancelText="확인"
+              onCancel={() => {
+                setIsPasswordError(false);
+                setIsPasswordModalOpen(true);
+              }}
+              isLightMode
+            >
+              비밀번호를 확인해주세요
+            </Modal>
+          )}
+        </>
       ) : (
         <MeetingRoom meetingId={meetingId} />
       )}
