51a96ea82ed5e6b3a39fbf6fa4d5bc66791b74b5
KimDwDev <dongwan123456789@gmail.com>
2026-02-04 14:40:15 +0900

    ✅ Test: room에 대해서 단위테스트를 진행

--- frontend/src/utils/createProduceHelpers.ts
+++ frontend/src/utils/createProduceHelpers.ts
@@ -58,9 +58,9 @@ export function createProduceHelper(sendTransport: Transport, device: Device) {
         appData: { type: 'screen_video' },
         codec: vp9,
         encodings: [
-          { maxBitrate: 4_000_000 }, // cam 보다는 더 높아야 한다. (상황에 따라서 SVC를 사용할 수 있게 해주면 좋다.) ( SVC는 좀 더 나중에 세밀한 조정때 유용하다. )
+          { maxBitrate: 5_000_000 }, // cam 보다는 더 높아야 한다. (상황에 따라서 SVC를 사용할 수 있게 해주면 좋다.) ( SVC는 좀 더 나중에 세밀한 조정때 유용하다. )
         ],
-        codecOptions: { videoGoogleStartBitrate: 1000 },
+        codecOptions: { videoGoogleStartBitrate: 1200 },
       });
     } // vp9은 실험을 통해서 진행을 해야 한다.
 
@@ -69,11 +69,11 @@ export function createProduceHelper(sendTransport: Transport, device: Device) {
       track,
       appData: { type: 'screen_video' },
       encodings: [ // 화면 공유에 경우 vp8인 경우 이러한 타입으로 진행을 한다.
-        { rid: "r0", scaleResolutionDownBy: 2, maxBitrate: 1_200_000 }, // 540p 
-        { rid: "r1", scaleResolutionDownBy: 1, maxBitrate: 5_000_000 }, // 1080p
+        { rid: "r0", scaleResolutionDownBy: 2, maxBitrate: 800_000 }, // 540p 
+        { rid: "r1", scaleResolutionDownBy: 1, maxBitrate: 3_500_000 }, // 1080p
       ],
       codecOptions: {
-        videoGoogleStartBitrate: 1000, // 화면공유에 시작 비트레이트는 낮게
+        videoGoogleStartBitrate: 1200, // 화면공유에 시작 비트레이트는 낮게
       },
     });
   };
